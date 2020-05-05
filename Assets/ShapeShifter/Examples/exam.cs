b143907843b2748e915770f8b06cad24e0961e85
m2wasabi <m2wasabi@gmail.com>
2020-05-05 12:25:30 +0900

    Load BlendShapeFrames

---
+++ Assets/ShapeShifter/Examples/exam.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ShapeShifter.Scripts;
+using UnityEngine;
+
+public class exam : MonoBehaviour
+{
+    public GameObject avatar;
+    private GameObject _distGo;
+    private SkinnedMeshRenderer[] _smrs;
+    private Mesh[] _meshes;
+    private static readonly string errMessageMeshReadable
+        = "Meshが読み込めません。fbxファイルの[ImportSettings] > [Model] > [Read/Write Enabled] を確認してください。";
+    void Start()
+    {
+        if(avatar == null) return;
+        _distGo = Instantiate(avatar);
+        _smrs = _distGo.GetComponentsInChildren<SkinnedMeshRenderer>();
+        _meshes = new Mesh[_smrs.Length];
+        var smrsCount = _smrs.Length;
+        for (int i = 0; i < smrsCount; i++)
+        {
+            if(!_smrs[i].sharedMesh.isReadable) throw new Exception(errMessageMeshReadable);
+            _meshes[i] = Instantiate(_smrs[i].sharedMesh);
+        }
+
+        var mesh = _meshes[5];
+        var blendCount = mesh.blendShapeCount;
+        var blendShapeFrameCounts = new List<int>();
+        var blendShapeFrames = new List<BlendShapeFrame>();
+        for (int i = 0; i < blendCount; i++)
+        {
+            // blendShapeFrameCount を取得する (大抵1である)
+            var bsc = mesh.GetBlendShapeFrameCount(i);
+            blendShapeFrameCounts.Add(bsc);
+            for (int j = 0; j < bsc; j++)
+            {
+                var frame = new BlendShapeFrame(mesh, i, j);
+                blendShapeFrames.Add(frame);
+            }
+        }
+        Debug.Log("See");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ShapeShifter/Scripts/BlendShapeFrame.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace ShapeShifter.Scripts
+{
+    [Serializable]
+    public class BlendShapeFrame
+    {
+        [SerializeField] public string name;
+        [SerializeField] public int shapeIndex;
+        [SerializeField] public int frameIndex;
+        [SerializeField] public Vector3[] vertices;
+        [SerializeField] public Vector3[] normals;
+        [SerializeField] public Vector3[] tangents;
+
+        public BlendShapeFrame(int vertexCount)
+        {
+            InitVectors(vertexCount);
+        }
+
+        public BlendShapeFrame(Mesh mesh, int shapeIndex, int frameIndex)
+        {
+            GetVertices(mesh, shapeIndex, frameIndex);
+        }
+
+        private void InitVectors(int vertexCount)
+        {
+            vertices = new Vector3[vertexCount];
+            normals = new Vector3[vertexCount];
+            tangents = new Vector3[vertexCount];
+        }
+
+        public void GetVertices(Mesh mesh, int shapeIndex, int frameIndex)
+        {
+            InitVectors(mesh.vertexCount);
+            this.shapeIndex = shapeIndex;
+            this.frameIndex = frameIndex;
+            mesh.GetBlendShapeFrameVertices(shapeIndex, frameIndex, vertices, normals, tangents);
+        }
+    }
+}
