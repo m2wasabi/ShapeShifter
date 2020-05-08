3a534c78f231b2f911111d977132fd3926626d81
m2wasabi <m2wasabi@gmail.com>
2020-05-08 23:51:49 +0900

    Begin core script

---
+++ Assets/ShapeShifter/Scripts/ShapeShifter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace ShapeShifter.Scripts
+{
+    public class ShapeShifter
+    {
+        private GameObject _prefab;
+        private SkinnedMeshRenderer _smr;
+        private Mesh _srcMesh;
+        
+    }
+
+    public class BlendRatio
+    {
+        public int Index;
+        public float Weight;
+    }
+}
